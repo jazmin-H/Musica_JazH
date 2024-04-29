9618d59a090bd5c4a91fc501de8c0f3d3169eb31
jazmin-H <huaynocajazmin999@email.com>
2024-04-29 12:08:47 -0300

    hasta el ejercicio 2 y el 3 esta en proceso

--- canciones/__init__.py
+++ canciones/__init__.py
@@ -1,11 +1,17 @@
-
-
 from flask import Flask
 
 app = Flask(__name__)
+
+with app.app_context():
+
+  from db import init_app
+  init_app(app)
+#importo esa y la llamo#
 
 
 @app.route('/')
 def hello():
     return 'Hello, World!'
+
+
 
